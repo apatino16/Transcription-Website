364da0ba7067d28582936f6242ddedac5a009add
apatino16 <a.a.patino16@gmail.com>
2022-04-30 15:01:08 -0700

    Tried connecting to firebase

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// ---------------------- Firebase --------------//
+const firebase_app_js_1 = require("https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js");
+const firebase_analytics_js_1 = require("https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js");
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD4ziKkzq2Bv2VjLeg7LvBNC1eZUB0yQh0",
+    authDomain: "transcription-website-55f1b.firebaseapp.com",
+    databaseURL: "https://transcription-website-55f1b-default-rtdb.firebaseio.com",
+    projectId: "transcription-website-55f1b",
+    storageBucket: "transcription-website-55f1b.appspot.com",
+    messagingSenderId: "357514451132",
+    appId: "1:357514451132:web:13a0af8d3f9569eb6d18b9",
+    measurementId: "G-07Q0DWGX8G"
+};
+// Initialize Firebase
+const app = firebase_app_js_1.initializeApp(firebaseConfig);
+const analytics = firebase_analytics_js_1.getAnalytics(app);
 // ------------------Log-In.html------------------//
 //Show/Hide Password Mask
 function viewPassword() {
