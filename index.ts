adbc733c16bc3390b533f340cb2bb84aecb390f3
apatino16 <a.a.patino16@gmail.com>
2022-04-30 16:12:46 -0700

    Styled the log in button and added a signup now anchor

--- index.ts
+++ index.ts
@@ -1,25 +1,41 @@
-// ---------------------- Firebase --------------//
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// // ---------------------- Firebase --------------//
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
+// import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js";
+//   // Your web app's Firebase configuration
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyD4ziKkzq2Bv2VjLeg7LvBNC1eZUB0yQh0",
+//     authDomain: "transcription-website-55f1b.firebaseapp.com",
+//     databaseURL: "https://transcription-website-55f1b-default-rtdb.firebaseio.com",
+//     projectId: "transcription-website-55f1b",
+//     storageBucket: "transcription-website-55f1b.appspot.com",
+//     messagingSenderId: "357514451132",
+//     appId: "1:357514451132:web:13a0af8d3f9569eb6d18b9",
+//     measurementId: "G-07Q0DWGX8G"
+//   };
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyD4ziKkzq2Bv2VjLeg7LvBNC1eZUB0yQh0",
-    authDomain: "transcription-website-55f1b.firebaseapp.com",
-    databaseURL: "https://transcription-website-55f1b-default-rtdb.firebaseio.com",
-    projectId: "transcription-website-55f1b",
-    storageBucket: "transcription-website-55f1b.appspot.com",
-    messagingSenderId: "357514451132",
-    appId: "1:357514451132:web:13a0af8d3f9569eb6d18b9",
-    measurementId: "G-07Q0DWGX8G"
-  };
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+//   const auth = getAuth(app);
+
+//   document.getElementById("login-btn")?.addEventListener('click', function(){
+//     const loginEmail = document.getElementById("email")?.value;  
+//     const loginPassword = document.getElementById("password-field")?.value; 
+
+//     signInWithEmailAndPassword(auth, email, password-field)
+//    .then((userCredential) => {
+//     // Signed in 
+//     const user = userCredential.user;
+//     // ...
+//   })
+//    .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//   });
+
+// });
 
 // ------------------Log-In.html------------------//
 //Show/Hide Password Mask
